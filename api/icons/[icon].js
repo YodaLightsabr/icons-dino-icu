40c234f4372f5660f96f5507b1389aa7ebfdd699
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-04-01 18:27:40 -0700

    fix: change route and add alias for hex colors

--- api/icons/[icon].js
+++
@@ -1,7 +0,0 @@
-import IconLibrary from '../../icons.js';
-
-export default async function handler(request, response) {
-    let { icon } = request.query;
-    if (icon.endsWith('.svg')) icon = icon.substring(0, icon.length - 4);
-    response.end(IconLibrary.icon(icon));
-}
--- api/icons/colors/[color]/glyphs/[glyph].js
+++ api/icons/colors/[color]/glyphs/[glyph].js
@@ -2,6 +2,7 @@ import IconLibrary from '../../../../../icons.js';
 
 export default async function handler(request, response) {
     let { color, glyph } = request.query;
+    if (color.startsWith('0x') && color.length === 8) color = '#' + color.substring(2);
     if (glyph.endsWith('.svg')) glyph = glyph.substring(0, glyph.length - 4);
     response.end(IconLibrary.icon(glyph, undefined, color));
 }
