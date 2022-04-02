f423a34783149c985d9220a1afdf7bfd36284dab
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-04-01 19:06:41 -0700

    fix: use nodejs default

--- api/icons/colors/[color]/glyphs/[glyph].js
+++ api/icons/colors/[color]/glyphs/[glyph].js
@@ -2,7 +2,7 @@ import IconLibrary from '../../../../../icons.js';
 
 export default async function handler(request, response) {
     let { color, glyph } = request.query;
-    response.type('image/svg+xml');
+    response.setHeader('Content-Type', 'image/svg+xml');
     if (color.startsWith('0x') && color.length === 8) color = '#' + color.substring(2);
     if (glyph.endsWith('.svg')) glyph = glyph.substring(0, glyph.length - 4);
     response.end(IconLibrary.icon(glyph, undefined, color));
