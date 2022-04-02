93b9582becb78d51c49f9682bea83adc6af1b42b
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-04-01 18:15:56 -0700

    feat: add support for .svg

--- api/icons/[icon].js
+++ api/icons/[icon].js
@@ -1,6 +1,7 @@
 import IconLibrary from '../../icons.js';
 
 export default async function handler(request, response) {
-    const { icon } = request.query;
+    let { icon } = request.query;
+    if (icon.endsWith('.svg')) icon = icon.substring(0, icon.length - 4);
     response.end(IconLibrary.icon(icon));
 }
