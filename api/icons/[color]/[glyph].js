bb11031ad12e39e1c41ee77108a7a81c99bef9c6
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-04-02 09:45:12 -0700

    feat: change api route structure

--- api/icons/[color]/[glyph].js
+++ api/icons/[color]/[glyph].js
@@ -1,9 +1,14 @@
-import IconLibrary from '../../../../../icons.js';
+import IconLibrary from '../../../icons.js';
 
 export default async function handler(request, response) {
     let { color, glyph } = request.query;
+    if (color.startsWith('color:')) color = color.substring(6);
+    if (glyoh.startsWith('glyph:')) glyph = glyph.substring(6);
     response.setHeader('Content-Type', 'image/svg+xml');
     if (color.startsWith('0x') && color.length === 8) color = '#' + color.substring(2);
     if (glyph.endsWith('.svg')) glyph = glyph.substring(0, glyph.length - 4);
     response.end(IconLibrary.icon(glyph, undefined, color));
 }
+
+// What a request should look like:
+// https://icons.dino.icu/icons/color:red/glyph:channels.svg
