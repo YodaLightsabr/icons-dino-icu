49a7518d56e6e32cb7cf45c4269c7a4d9df93fbd
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-04-02 09:47:43 -0700

    fix: I am stupid

--- api/icons/[color]/[glyph].js
+++ api/icons/[color]/[glyph].js
@@ -3,7 +3,7 @@ import IconLibrary from '../../../icons.js';
 export default async function handler(request, response) {
     let { color, glyph } = request.query;
     if (color.startsWith('color:')) color = color.substring(6);
-    if (glyoh.startsWith('glyph:')) glyph = glyph.substring(6);
+    if (glyph.startsWith('glyph:')) glyph = glyph.substring(6);
     response.setHeader('Content-Type', 'image/svg+xml');
     if (color.startsWith('0x') && color.length === 8) color = '#' + color.substring(2);
     if (glyph.endsWith('.svg')) glyph = glyph.substring(0, glyph.length - 4);
